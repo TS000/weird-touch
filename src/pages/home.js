8eccd466db333cd17da19d717a2d9c98b31a9346
TS000 <tylersnell@me.com>
2019-03-08 16:01:04 -0700

    fetch fix

--- src/pages/home.js
+++ src/pages/home.js
@@ -9,9 +9,6 @@ const CardWrap = styled.div`
   justify-content: center;
   flex-wrap: wrap; 
 `
-const mixes = [ 
-  {id: 'mix1', name: 'Cool Mix1', image: 'https://dmcyn88lu74jl.cloudfront.net/images/mix4.png'}
-];
 
 const url = 'https://dmcyn88lu74jl.cloudfront.net/json/list.json';
 
@@ -22,30 +19,38 @@ const data = {
   }),
 };
 
-fetch(url,data)
-.then(response => response.json())
-      .then(json => {
-        console.log(json)
-      })
 
 
 export default class Home extends Component {
-  state = { showPlayer: false }
+  constructor(props) {
+    super(props);
+    this.state = { mixes: [] }
+  }
+
+  componentDidMount(){
+    const { mixes } = this.state;
+    fetch(url,data)
+      .then(response => response.json())
+      .then(json => {
+        this.setState({ mixes: json });
+        console.log(mixes)
+      })
+    }
 
   render() { 
-    const { showPlayer } = this.state;
+    const { mixes } = this.state;
+    console.log(mixes)
     return ( 
       <React.Fragment>
         <h1>Weird Touch.</h1>
         <CardWrap>
         {mixes.map(mix => (
         <Card 
-            innerTitle = { mix.name }
+            innerTitle = { mix.dj }
             img={ mix.image }
             imgAlt="a thing"
             white
-            key={ mix.name }
-            showPlayer={ showPlayer }
+            key={ mix.dj }
             id={ mix.id }
         />
         ))}
@@ -54,3 +59,4 @@ export default class Home extends Component {
      );
   }
 }
+
